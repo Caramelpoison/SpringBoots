2537d85d6e486ce0ac344a592dc51fb89c682a0f
caramel <AlexanderForward149@gmail.com>
2023-07-28 10:21:16 -0400

    aws upload

---
+++ TDDorders/src/main/java/com/example/ordermanagement/TddAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddAssignmentApplication.class, args);
+	}
+
+}
